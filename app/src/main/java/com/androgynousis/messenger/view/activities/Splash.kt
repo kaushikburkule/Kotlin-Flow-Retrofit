4861cd6179ae36a081848d6054f60d58bdfd7e90
Kaushik Burkule <kaushikburkule@gmail.com>
2020-06-25 22:30:35 +0530

    min android api level is 23 now
    Signed-off-by: Kaushik Burkule <kaushikburkule@gmail.com>

--- app/src/main/java/com/androgynousis/messenger/view/activities/Splash.kt
+++ app/src/main/java/com/androgynousis/messenger/view/activities/Splash.kt
@@ -14,9 +14,7 @@ class Splash : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-        if (Build.VERSION.SDK_INT >= 21) {
-            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-        }
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
         changeStatusBarColor()
         Handler().postDelayed({
             startActivity(Intent(this@Splash, SetupPhoneNumber::class.java))
@@ -25,10 +23,9 @@ class Splash : AppCompatActivity() {
     }
 
     private fun changeStatusBarColor() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            val window = window
-            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
-            window.statusBarColor = Color.TRANSPARENT
-        }
+        val window = window
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        window.statusBarColor = Color.TRANSPARENT
     }
+
 }
