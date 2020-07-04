6ba50f668e68f69a6ba7383edc1f643d4efc9ed1
Kaushik Burkule <kaushikburkule@gmail.com>
2020-07-04 23:49:44 +0530

    Routine commit
    Signed-off-by: Kaushik Burkule <kaushikburkule@gmail.com>

--- app/src/main/java/com/androgynousis/messenger/view/SplashActivity.kt
+++ app/src/main/java/com/androgynousis/messenger/view/SplashActivity.kt
@@ -13,16 +13,12 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-        changeStatusBarColor()
-        Handler().postDelayed({
-            finish()
-        }, 5 * 1000.toLong())
-    }
-
-    private fun changeStatusBarColor() {
         val window = window
         window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
         window.statusBarColor = Color.TRANSPARENT
+        Handler().postDelayed({
+            finish()
+        }, 5 * 1000.toLong())
     }
 
 }
